ff1562cd9768ddff2dbd2586f5f9d5f33635494a
hanodi <hanodims@hotmail.com>
2020-08-16 11:52:43 +0300

    madlibs.py add

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,18 @@
 def main():
 	# write your code here
+	print('Mad lips where lips get mad. \nStart below:')
+	time = input('Enter a number from 1 to 12: ')
+	items = input('Enter a noun (plural): ')
+	name = input('Enter a name: ')
+	scream = input('Enter any sentence: ')
+	action = input('Enter a verb: ')
+	print("\n the story goes...")
+	print()
+	print('It was %s o\'clock when I heard a knock at the door.' % time)
+	print('I opend the door and there was a box full of %s with a note saying "From Mr. %s."' % (items, name.upper()) )
+	print('Just as I closed the door I heard a scream "%s."' % (scream.upper()))
+	print('I froze in place and all I could do was %s' % (action) )
+
 
 
 
